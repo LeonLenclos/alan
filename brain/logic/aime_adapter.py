342421a8ad5c93aa5d95a89e288bbaeff83b0d0a
fabturing <34024298+fabturing@users.noreply.github.com>
2018-03-08 15:49:34 +0100

    Revert "Dev fab"

--- brain/logic/aime_adapter.py
+++ brain/logic/aime_adapter.py
@@ -2,7 +2,6 @@ from logic import AlanLogicAdapter
 from chatterbot.conversation import Statement
 import utils
 import re
-import random
 
 class AimeAdapter(AlanLogicAdapter):
     """This adapter answer to questions about what someone or something loves,
@@ -15,11 +14,19 @@ class AimeAdapter(AlanLogicAdapter):
     def __init__(self, **kwargs):
         """Required kwarg :
 
+        questions
+        A list of strings.
+        The question that must be answered with this adapter.
 
         relation
         A list of strings.
         The nature of the questions that must be answered with this adapter
 
+        ask
+        A list of strings
+        The question used to ask for a relation for an unknown concept.
+        Ask will include a specifier for the concept which is asked :
+        "Pourrais tu me dire en quelques mots ce qu'est %(concept_A)s."
          """
 
 
@@ -38,91 +45,76 @@ class AimeAdapter(AlanLogicAdapter):
     def can_process(self, statement):
         # Process only if the latest statement in the conversation
         # contain the relation
-        if self.relation in statement.text :
-            if "je" not in re.split("[Aa]ime",statement.text)[0] :
-                if "j'" not in re.split("[Aa]ime",statement.text)[0] :
-                    return True
-        else :
-            return False
-
-
+        return (self.relation in statement.text)
 
     def process(self, statement):
-        # Get the relation
         relation=self.relation
-        if (re.match("([Aa]ime[s]*-)|([Aa]ime[s]*[- ]tu )|([Aa]ime[s]*[- ]t[- ]il)",
-                                                        statement.text)) :
-            if re.match("[Aa]ime[s]*[ -]tu ", statement.text) :
-                concept_A="tu"
-                concept_B=re.sub(".*[Aa]ime[s][- ]tu ","",statement.text)
-                concept_B=utils.remove_punctuation(concept_B, False)
-            elif re.match("[Aa]ime[s]*[- ]t[- ]il", statement.text) :
-                concept_A=re.split("[Aa]ime[s]*[- ]t[- ]il",statement.text)[0]
-                concept_B=re.split("[Aa]ime[s]*[- ]t[- ]il",statement.text)[1]
-            else :
-                statment_out = Statement()
-                statment_out.confidence = self.get_confidence(0)
-                return statment_out
+        
+        # concept_A is the chain before the last relation occurence
+        concept_A = re.sub(relation+".*([ '])","",statement.text)
+        # Here, we also remove the "est ce que" expression
+        concept_A = re.sub("([eE]st[ -]ce[ -]que)","",statement.text)
+        # Remove the punctuation from concept_A except apostrophe "'"
+        concept_A=utils.remove_punctuation(concept_A, False)
+        # Remove the chain " quoi " if it begin concept_A (because of "C'est
+        # quoi..." questions)
+        concept_A = re.sub("^(quoi)","",concept_A)
+        # Remove starting and ending spaces
+        concept_A=concept_A.strip()
+        # Get the interrogative part of the question that is before the concept_A
+        question = statement.text.split(concept_A)[0]
 
+        # The following block allow the kezako adapter to answer to the "Qu'est
+        # ce que..." and "Qu'est ce qu'..." questions.
+        # Because this questions can have another meaning if a verb follow the
+        # question e.g in "Qu'est ce que tu fais"
+        # If you uncomment the block, don't forget to add this two questions to
+        # the questions into the settings.json file
+        #   #Remove the chain " ce que " from concept_A (because of "Qu'est ce
+        #    que..." questions)
+        #   concept_A = re.sub("^( ce que )","",concept_A)
+        #   #Remove the chain " ce qu' " from concept_A (because of "Qu'est ce
+        #    qu'..." qu'est)
+        #   concept_A = re.sub("^( ce qu')","",concept_A)
+
+        # Get the distance between input statement and questions list
+        confidence = utils.compare(question, self.questions)
+
+
+        # If concept_A is related by the relation to another concept, put
+        # this concept into concept_B
+        if self.chatbot.storage.get_related_concept(concept_A, self.relation):
+            concept_B=self.chatbot.storage.get_related_concept(concept_A,
+                                                                relation)
+            # Turn the first letter of the concept_A chain to a capital
+            concept_A = concept_A.lower().capitalize()
+            # Answer the question
+            response = concept_A+" "+relation+" "+concept_B+"."
+
+        # If a concept is related to concept_A by the relation, put
+        # this concept into concept_C
+        elif  self.chatbot.storage.get_related_concept(concept_A, relation,
+                                                        reverse=True):
+            concept_C = self.chatbot.storage.get_related_concept(concept_A,
+                                                        relation, reverse=True)
+            # Turn the first letter of the concept_B chain to a capital
+            concept_C = concept_C.lower().capitalize()
+            # Answer and ask
+            response = concept_C+" "+relation+" "+concept_A+\
+            " mais je ne sais pas vraiment ce qu'est "+concept_A+". "\
+            +self.ask % {"concept_A":concept_A}
+        # Else aske for a concept related to concept_A
         else:
-            concept_A=re.split(self.relation,statement.text)[0]
-            concept_A=utils.remove_punctuation(concept_A, False)
-            # Here, we also remove the "est ce que" expression
-            concept_A = re.sub("([eE]st[ -]ce[ -]que)","",concept_A)
-            concept_B=re.split(self.relation+"[s]*",statement.text)[1]
-            concept_B=utils.remove_punctuation(concept_B, False)
-        confidence = 1
-
-        # Here we operate a magic substitution in order to change who is talking
-        # e.g we change "tu" by "je"
-        concept_A = utils.magic_sub(concept_A)
-        concept_B = utils.magic_sub(concept_B)
-        concept_C = self.chatbot.storage.get_related_concept(concept_B,
-                                            self.relation, reverse=True)
-
-        # If concept_A is related by the relation to concept_B
-        if (self.chatbot.storage.is_related_concept(concept_A, self.relation,
-                                                    concept_B) is not None) :
-            concept_A = concept_A.capitalize()
-            if self.chatbot.storage.is_related_concept(concept_A, self.relation,concept_B):
-                response = "%(A)s %(rel)s %(B)s."
-            else:
-                response = "%(A)s n'%(rel)s pas %(B)s."
-        elif concept_A == "Alan":
-            if random.random()<0.5:
-                self.chatbot.storage.store_concept_association(concept_A,
-                                                            "aime", concept_B)
-                response="Oh oui j'%(rel)s %(B)s. Et toi?"
-            else:
-                self.chatbot.storage.store_concept_association(concept_A,
-                                    self.relation, concept_B, negative=True)
-                response="Pour être franc %(A)s n'%(rel)s pas %(B)s. Et toi?"
-        else:
-            # If some C is related to B by the relation
-            if self.chatbot.storage.get_related_concept(concept_B,
-             self.relation, reverse=True) :
-
-                response = " Je sais que %(C)s %(rel)s %(B)s, mais je ne sais\
-                 pas si %(A)s (rel)s %(B)s, tu crois que c'est le cas?"
-            elif self.chatbot.storage.get_related_concept(concept_B,
-             self.relation, reverse=True) is False :
-                  response = " Je sais que %(C)s n'%(rel)s pas %(B)s, mais je\
-                   ne sais pas si %(A)s (rel)s %(B)s, tu crois que c'est le cas?"
-            else:
-                response = " Et bien écoute je ne sais\
-pas si %(A)s %(rel)s %(B)s, tu crois que c'est le cas?"
-
-        response = response % {"A":concept_A, "B":concept_B, "C":concept_C,
-        "rel":self.relation }
-        response = re.sub("Alan aime","j'aime",response)
-        response = re.sub("Alan n'aime","je n'aime",response)
+            # Answer and ask
+            response = "Je ne sais pas ce qu'est "+concept_A+". "+\
+            self.ask % {"concept_A":concept_A}
 
         # Verify that concept_A is non-empty or to big (more than 4 words),
         #  if it is then change confidence to 0
         if len(concept_A) == 0 or len(concept_A.split(" "))>4:
             confidence=0
 
-
-        statment_out = Statement(response.capitalize())
+        statment_out = Statement(response)
         statment_out.confidence = self.get_confidence(confidence)
+
         return statment_out
