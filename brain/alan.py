bec26625db1fbb3ac26bf688677551f8ebc125ea
fabturing <fabien.carbo@protonmail.com>
2018-03-09 14:52:55 +0100

    audio musique generative + ajout de cest aux compiments

--- brain/alan.py
+++ brain/alan.py
@@ -6,8 +6,9 @@ import re
 import os
 import sys
 import datetime
-
+import wave
 import chatterbot
+import pygame
 
 from logic import MainLogicAdapter
 from test.simple_talk import test
@@ -58,6 +59,11 @@ class Alan(chatterbot.ChatBot):
         # Alan system attributes
         self.last_results=[]
         self.user_name = None
+
+        # init pygame.mixer in order to play wav sounds
+        pygame.mixer.init()
+        # create sound objects that Alan can play
+        self.musique_generative = pygame.mixer.Sound("./ressources/musique_generative.wav")
 
         # init chatterbot
         super().__init__(self.name, **settings)
@@ -144,7 +150,8 @@ class Alan(chatterbot.ChatBot):
                     infos += "NOT PROCESSING"
             infos += "\n---\n"
             print(infos)
-
+        if command == "music":
+            self.musique_generative.play()
 
     def learn_response(self, statement, previous_statement):
         """
