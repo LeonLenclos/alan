78dcf64201bc037b605601b8002b4b37406168b8
LeonLenclos <leon.lenclos@gmail.com>
2018-04-30 17:28:30 +0200

    on enleve la ponctuation pour rivescript

--- brain/logic/rivescript_adapter.py
+++ brain/logic/rivescript_adapter.py
@@ -42,14 +42,14 @@ class RiveScriptAdapter(AlanLogicAdapter):
     def get(self, statement):
         """take a statment and ask a reply to the interpreter"""
         user = "human"
-        text = statement.text
+        text = remove_punctuation(statement.text, False)
 
         # set last reply as the real reply
         history = self.interpreter.get_uservar(user, "__history__")
         if type(history) is dict:
             latest_reply = self.chatbot.storage.get_latest_statement()
             if latest_reply:
-                history["reply"][0] = latest_reply.text
+                history["reply"][0] = remove_punctuation(latest_reply.text, False)
         self.interpreter.set_uservar(user, "__history__", history)
 
         # if self.reply is empty, get a reply if not return the last reply
