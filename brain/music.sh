6f59af530d73818ab3aa67dc5d0cb9d5c6a47a85
Bertrand Lenclos <bebert@nokill.fr>
2018-11-20 10:24:30 +0100

    ber matin 2

--- brain/music.sh
+++ brain/music.sh
@@ -3,11 +3,11 @@ sox tmp.wav tmp2.wav speed $1
 n=1000; while [ $n -gt 400 ]; do beep -f $n -l 5; n=$((n*97/100)); done
 n=2000; while [ $n -gt 400 ]; do beep -f $n -l 5; n=$((n*97/100)); done
 n=3000; while [ $n -gt 400 ]; do beep -f $n -l 5; n=$((n*97/100)); done
-play -q tmp.wav -t alsa
+aplay -q tmp.wav
 beep -f 1000 -n -f 1500 -n -f 600 -n -f 500 -n -f 100 -r 2 -l 10 -n -f 50 -r 2 -l 200 -n -f 40 -r 2 -l 300 -n -f 60 -r 3 -n -f 50 -r 3
-play -q tmp.wav -t alsa
-play -q tmp.wav -t alsa
-play -q tmp.wav -t alsa
+aplay -q tmp.wav
+aplay -q tmp.wav
+aplay -q tmp.wav
 n=1000; while [ $n -gt 400 ]; do beep -f $n -l 5; n=$((n*97/100)); done
 n=2000; while [ $n -gt 400 ]; do beep -f $n -l 5; n=$((n*97/100)); done
 n=3000; while [ $n -gt 400 ]; do beep -f $n -l 5; n=$((n*97/100)); done
