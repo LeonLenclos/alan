0040838ce094d33591c91eeba452dffc158530d4
Fab <fab@buse.lan>
2018-11-22 14:36:12 +0100

    bug alsa fixé pour de bon ! fichiers d'erreur et fichiers wav enregistrés dans/tmp

---
+++ brain/test.sh
@@ -0,0 +1,10 @@
+if [ -f error ]
+then
+	echo 1
+fi
+
+if [ -f errors ]
+then
+	echo 3
+fi
+
--- brain/voice_audio.sh
+++ brain/voice_audio.sh
@@ -1,3 +1,9 @@
-pico2wave -l fr-FR -w tmp.wav "$1"
-play -q tmp.wav -t alsa speed $2
-rm tmp.wav
+pico2wave 2>/tmp/error_pico -l fr-FR -w /tmp/alan_voice.wav "$1"
+play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2
+while [ -s /tmp/error_alsa ]
+do
+        rm /tmp/error_alsa
+        sleep 0.01
+        play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2
+
+done
