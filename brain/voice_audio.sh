9f236d253f23f7cb0756864dc7f999df3651eb5c
Léon Lenclos <leon.lenclos@gmail.com>
2019-04-10 19:16:01 +0200

    doc

--- brain/voice_audio.sh
+++ brain/voice_audio.sh
@@ -1,3 +1,6 @@
+# Usage : ./voice_audio.sh text speed
+# Dependencies : pico-tts, sox
+
 pico2wave 2>/tmp/error_pico -l fr-FR -w /tmp/alan_voice.wav "$1"
 play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2
 while [ -s /tmp/error_alsa ]
