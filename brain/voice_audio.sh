d21e3b403b3a919e683840c3b6cbdadc64c04da1
fabturing <34024298+fabturing@users.noreply.github.com>
2019-04-10 17:45:38 +0000

    Update voice_audio.sh

--- brain/voice_audio.sh
+++ brain/voice_audio.sh
@@ -2,6 +2,7 @@
 # Dependencies : pico-tts, sox
 
 pico2wave 2>/tmp/error_pico -l fr-FR -w /tmp/alan_voice.wav "$1"
+# play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2 highpass frequency 500 
 play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2
 while [ -s /tmp/error_alsa ]
 do
