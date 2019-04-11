62e6906571170a9ecda70250130195f507a409fd
Leon Lenclos <leon.lenclos@gmail.com>
2019-04-11 21:29:21 +0200

    filtre voix

--- brain/voice_audio.sh
+++ brain/voice_audio.sh
@@ -2,13 +2,13 @@
 # Dependencies : pico-tts, sox
 
 pico2wave 2>/tmp/error_pico -l fr-FR -w /tmp/alan_voice.wav "$1"
-play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2 highpass 500 
+play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2 highpass 600
 #play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2
 while [ -s /tmp/error_alsa ]
 do
         rm /tmp/error_alsa
         sleep 0.01
-        play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2 highpass 500 
+        play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2 highpass 600
         #play 2>/tmp/error_alsa -q /tmp/alan_voice.wav -t alsa speed $2
 
 done
