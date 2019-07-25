c9454690531f85bfb054c584139164bf05c0c66a
Léon Lenclos <leon.lenclos@gmail.com>
2019-07-25 20:37:25 +0200

    un gros bordel avec plein de modif dans tous les sens

--- brain/logic/best_practices.py
+++ brain/logic/best_practices.py
@@ -6,7 +6,9 @@ from utils import compare
 from utils import french_dict
 
 class BestPracticesAdapter(AlanLogicAdapter):
-    """This logic adapter search for past statments"""
+    """This logic adapter search for past statments
+        requieres french dict in utils
+    """
 
     def __init__(self, **kwargs):
         """Required kwarg :
@@ -17,6 +19,8 @@ class BestPracticesAdapter(AlanLogicAdapter):
         Optionals :
 
         min_misspelled
+
+        max_wellspelled
 
         min_letters
 
@@ -31,16 +35,21 @@ class BestPracticesAdapter(AlanLogicAdapter):
         if type(self.sentence) != str:
             raise TypeError("questions must be a sentence")
 
-        self.min_misspelled = kwargs.get('min_misspelled', 3)
+        self.min_misspelled = kwargs.get('min_misspelled', 0)
+        self.min_misspelled = kwargs.get('max_wellspelled', 100)
         self.min_letters = kwargs.get('min_letters', 20)
 
     def can_process(self, statement):
+        if not french_dict :
+            return False
+
         letters = len(statement.text)
-        misspelled = 0
-        if french_dict :
-            misspelled = sum(1 for w in word_tokenize(statement.text)
-                             if french_dict.check(w))
-        return misspelled > self.min_misspelled or letters > self.min_letters
+
+        misspelled = sum(1 for w in word_tokenize(statement.text)
+                         if not french_dict.check(w))
+        wellspelled = len(word_tokenize(statement.text)) - misspelled
+
+        return misspelled >= self.min_misspelled and wellspelled <= self.max_wellspelled and letters >= self.min_letters
 
     def process(self, statement):
         statement_out = Statement(self.sentence)
