a2bc6103fc1a738ff1551db04e0c93cd38fb4256
Léon Lenclos <leon.lenclos@gmail.com>
2020-04-29 20:50:20 +0200

    proposition de preprocessor pour emoticon

--- brain/preprocessors.py
+++ brain/preprocessors.py
@@ -28,3 +28,11 @@ def add_speaker_data(chatbot, statement):
     """Add 'human' to extra_data['speaker']"""
     statement.add_extra_data("speaker", "human")
     return statement
+
+def sub_emoticon(chatbot, statement):
+    """substitute a smiley by a 'smiley'"""
+    emoticons = {'emoticonheart': ('<3',), 'emoticonother': ('O_O', 'o‑o', 'O_o', 'o_O', 'o_o', 'O-O', '>.<', ':-*', ':*', ':×', ':‑O', ':O', ':‑o', ':o', ':-0', '8‑0', '>:O', ':‑/', ':/', ':‑.', '>:\\', '>:/', ':\\', '=/', '=\\', ':L', '=L', ':S', ':‑|', ':| ', ':$', '://)', '://3', ':‑X', ':X', ':‑#', ':#', ':‑&', ':&', 'O:‑)', 'O:)', '0:‑3', '0:3', '0:‑)', '0:)', '0;^)', '>:‑)', '>:)', '}:‑)', '}:)', '3:‑)', '3:)', '>;)', '>:3', '>;3 ', '|;‑)', '|‑O ', ':‑J'), 'emoticonhappy': (':‑)', ':)', ':-]', ':]', ':-3', ':3', ':->', ':>', '8-)', '8)', ':-}', ':}', ':o)', ':c)', ':^)', '=]', '=)', ':‑D', ':D', '8‑D', '8D', 'x‑D', 'xD', 'X‑D', 'XD', '=D', '=3', 'B^D', ":'‑)", ":') ", ';‑)', ';)', '*-)', '*)', ';‑]', ';]', ';^)', ':‑,', ';D', ':^)'), 'emoticonsad': (':‑(', ':(', ':‑c', ':c', ':‑<', ':<', ':‑[', ':[', ':-||', '>:[', ':{', ':@', '>:(', ';(', ":'‑(", ":'( ", ':^(')}
+    for emoticon_repr, emoticon_list in emoticons.items():
+        for emoticon in emoticon_list:
+            statement.text.replace(emoticon, emoticon_repr)
+    return statement
